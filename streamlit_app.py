075e84843567312aea349eb27fe0dd31ab20efb5
Lauren Gracias <lgracias2022@gmail.com>
2026-02-08 15:32:21 -0500

    update_hwmanager

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ hw1 = st.Page("hws/HW1.py", title="HW 1", icon="📄")
 hw2 = st.Page("hws/HW2.py", title="HW 2 - URL Summarizer", icon="🌐")
 hw3 = st.Page("hws/HW3.py", title="HW 3 - URL Chatbot", icon=":material/description:", default=True)
 
-pg = st.navigation([hw1, hw2])
+pg = st.navigation([hw1, hw2, hw3])
 
 st.set_page_config(page_title="HW Manager", page_icon="📚")
 
